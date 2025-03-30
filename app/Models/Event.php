fe907b1a598064aebd632e8969a94945c2d4ea38
andrepfdev <dreletras@gmail.com>
2025-03-30 20:45:13 -0300

    feat: modal e migration

---
+++ app/Models/Event.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Event extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'date',
+        'location',
+        'capacity',
+        'status',
+    ];
+    
+    protected $casts = [
+        'date' => 'datetime',
+        'capacity' => 'integer',
+    ];
+}
