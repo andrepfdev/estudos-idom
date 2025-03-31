ced696c8f661c55d7632dd30a26234930b285b8e
JP <joaojpwa@gmail.com>
2025-03-30 21:28:55 -0300

    feat: store method

--- app/Http/Controllers/Events.php
+++ app/Http/Controllers/Events.php
@@ -19,7 +19,36 @@ class Events extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $Rules = [
+            'title' => 'required|min:3|max:40',
+            'description' => 'required|min:10|max:250',
+            'date' => 'required',
+            'location' => 'required|min:3|max:40',
+            'capacity' => 'required',
+            'status' => 'required',
+        ];
+
+        $Feedback = [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'description.min' => 'O campo deve conter mais de 3 caracteres',
+            'location.min' => 'O campo deve conter mais de 3 caracteres',
+            'description' => 'O campo deve conter mais de 10 caracteres'
+        ];
+
+        $request->validate($Rules, $Feedback);
+
+        $events = $this->events->create([
+        'user_id' => $request->user_id,
+        'title'=> $request->title,
+        'description' => $request->description,
+        'date' => $request->date,
+        'location' => $request->location,
+        'capacity' => $request->capacity,
+        'status' => $request->status,
+        ]);
+
+
+        return response()->json($events, 201);
     }
 
     /**
